688867e5d8a20f5e932171e8e6a765e7a96637bc
Fraser Sharp <fraserrsharp@gmail.com>
2016-02-05 12:17:53 +0000

    First commit, basic test scene with movable stick object.

---
+++ Assets/Scripts/Lightbike.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lightbike : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame 
+    void Update () { 
+        GetComponent<Rigidbody>().velocity = new Vector3(Input.GetAxis("Horizontal")*20, 0, Input.GetAxis("Vertical")*20);
+	}
+}
