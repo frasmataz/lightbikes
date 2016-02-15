1de7dc063a7f4843613e3eedd74e602d1b49c585
Fraser Sharp <fraserrsharp@gmail.com>
2016-02-15 16:28:09 +0000

    Camera now follows player, and we produce a (slightly broken) light trail behind us

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public float followDistance;
+    public float cameraUpAngle;
+    public float cameraHeight;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject player = GameObject.Find("Cube");
+
+        Vector3 Diff = GetComponent<Transform>().position - player.GetComponent<Transform>().position;
+        if (Diff.magnitude > followDistance)
+        {
+            float mult = followDistance / Diff.magnitude;
+            GetComponent<Transform>().position -= Diff;
+            GetComponent<Transform>().position += Diff * mult;
+        }
+
+        GetComponent<Transform>().LookAt(player.GetComponent<Transform>());
+    }
+}
--- Assets/Scripts/Lightbike.cs
+++ Assets/Scripts/Lightbike.cs
@@ -2,14 +2,42 @@
 using System.Collections;
 
 public class Lightbike : MonoBehaviour {
+    public float speed;
+    public Material trailMaterial;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private SortedList path;
+    private Vector3 lastPath;
+    private GameObject lastTrail;
+
+    // Use this for initialization
+    void Start ()
+    {
+        lastTrail = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        path = new SortedList();
+        lastPath = GetComponent<Transform>().position;
+        lastTrail.GetComponent<BoxCollider>().enabled = false;
+        lastTrail.GetComponent<Renderer>().material = trailMaterial;
+    }
 	
 	// Update is called once per frame 
-    void Update () { 
-        GetComponent<Rigidbody>().velocity = new Vector3(Input.GetAxis("Horizontal")*20, 0, Input.GetAxis("Vertical")*20);
-	}
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<Transform>().localEulerAngles += new Vector3(0, -90, 0);
+            path.Add(path.Count, Object.Instantiate(lastTrail));
+            lastPath = GetComponent<Transform>().position;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            GetComponent<Transform>().localEulerAngles += new Vector3(0, 90, 0);
+            path.Add(path.Count, Object.Instantiate(lastTrail));
+            lastPath = GetComponent<Transform>().position;
+        }
+
+        GetComponent<Rigidbody>().velocity = GetComponent<Transform>().forward * speed;
+
+        Vector3 position = GetComponent<Transform>().position;
+        lastTrail.transform.position = (lastPath + position) / 2;
+        lastTrail.transform.localScale = new Vector3(position.x - lastPath.x, 5, position.z - lastPath.z);
+    }
 }
